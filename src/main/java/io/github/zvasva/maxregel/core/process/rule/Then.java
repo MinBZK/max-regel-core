86a88e7e37da8756e9d7d5d2e976d285b1736e18
Arvid Halma <ahalma@gmail.com>
2025-07-01 13:16:12 +0200

    Then only chains 2 rules if the first one is a) not a ReturnIf, or b) is a ReturnIf but fails. Otherwise the ReturnIf is evaluated and prevents further evaluation.

--- src/main/java/io/github/zvasva/maxregel/core/process/rule/Then.java
+++ src/main/java/io/github/zvasva/maxregel/core/process/rule/Then.java
@@ -30,6 +30,25 @@ public class Then extends AbstractRule {
 
     @Override
     public RuleResult apply(FactSet factset, Tracer tracer) {
+        // Skip identity rules to avoid unnecessary computation
+        if("identity".equals(a.op())){
+            return b.apply(factset, tracer);
+        }
+        if("identity".equals(b.op())){
+            return a.apply(factset, tracer);
+        }
+
+        if(a instanceof ReturnIf ri){
+            if(ri.condition(factset)) {
+                // Just return the result of the ReturnIf, don't apply b
+                return ri.result(factset, tracer);
+            } else {
+                // The condition was not met, so we can apply b
+                return b.apply(factset, tracer);
+            }
+        }
+
+        // The expected case: apply a, then b (with bookkeeping of updates and total results)
         RuleResult resultA = a.apply(factset, tracer);
         RuleResult resultB = b.apply(resultA.total(), tracer);
         FactSet update = resultA.update().union(resultB.update());
@@ -38,12 +57,25 @@ public class Then extends AbstractRule {
 
     @Override
     public FactSet apply(FactSet factset) {
+        // Skip identity rules to avoid unnecessary computation
         if("identity".equals(a.op())){
             return b.apply(factset);
         }
         if("identity".equals(b.op())){
             return a.apply(factset);
         }
+
+        if(a instanceof ReturnIf ri){
+            if(ri.condition(factset)) {
+                // Just return the result of the ReturnIf, don't apply b
+                return ri.result(factset);
+            } else {
+                // The condition was not met, so we can apply b
+                return b.apply(factset);
+            }
+        }
+
+        // The expected case: apply a, then b
         return b.apply(a.apply(factset));
     }
 }
